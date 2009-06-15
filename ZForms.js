80e52d2dc7544d2ec6b225aa9c44ff37f3f6d054
dfilatov81 <dfilatov81@378fa75c-80d0-11dd-8a74-1916a93a1be7>
2009-06-15 18:44:30 +0000

    init removed from ZForms.js in separate file
    git-svn-id: http://zforms-framework.googlecode.com/svn/trunk@54 378fa75c-80d0-11dd-8a74-1916a93a1be7

--- ZForms.js
+++ ZForms.js
@@ -424,39 +424,3 @@ var ZForms = {
 	}
 
 };
-
-// default init
-Common.Event.add(
-	document,
-	Common.Event.TYPE_DOM_CONTENT_LOADED,
-	function() {
-
-		var
-			aFormElements = Common.Dom.getElementsByClassName(
-				document,
-				ZForms.Builder.CLASS_NAME_WIDGET,
-				'form'
-				),
-			oElement,
-			i = 0,
-			aForms = []
-			;
-
-		while(oElement = aFormElements[i++]) {
-			if(!Common.Class.match(oElement, ZForms.Widget.Container.Form.CLASS_NAME_INITED)) {
-				aForms.push(ZForms.createBuilder(oElement).build());
-			}
-		}
-
-		setTimeout(
-			function() {
-
-				ZForms.notifyObservers(ZForms.EVENT_TYPE_ON_INIT, ZForms, aForms);
-				ZForms.bInited = true;
-
-			},
-			1
-			);
-
-	}
-	);
---
+++ init.js
@@ -0,0 +1,34 @@
+// default init
+Common.Event.add(
+	document,
+	Common.Event.TYPE_DOM_CONTENT_LOADED,
+	function() {
+
+		var
+			aFormElements = Common.Dom.getElementsByClassName(
+				document,
+				ZForms.Builder.CLASS_NAME_WIDGET,
+				'form'
+				),
+			oElement,
+			i = 0
+			;
+
+		while(oElement = aFormElements[i++]) {
+			if(!Common.Class.match(oElement, ZForms.Widget.Container.Form.CLASS_NAME_INITED)) {
+				ZForms.createBuilder(oElement).build();
+			}
+		}
+
+		setTimeout(
+			function() {
+
+				ZForms.notifyObservers(ZForms.EVENT_TYPE_ON_INIT, ZForms, aForms);
+				ZForms.bInited = true;
+
+			},
+			1
+			);
+
+	}
+	);
